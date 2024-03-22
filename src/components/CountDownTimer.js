8a59168ed233b3f1e3ff9fcdd5e1abad9cd17044
rajat858 <rajatics@gmail.com>
2024-03-22 06:10:20 +0530

    fixing edge cases

--- src/components/CountDownTimer.js
+++ src/components/CountDownTimer.js
@@ -1,55 +1,104 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./countDownTimer.css";
-function CountDownTimer({ selectedDateTime }) {
-
+function CountDownTimer({ selectedDateTime, setIsTimerOn, setDateTime }) {
+  const [isTimerOver, setIsTimerOver] = useState(false);
   const calculateTime = () => {
-
-    let timeLeft = {days: 0,
-                    hours: 0,
-                    minutes: 0,
-                    seconds: 0};
+    let timeLeftObj = { days: 0, hours: 0, minutes: 0, seconds: 0 };
     const currTime = new Date().getTime();
     const futureTime = new Date(selectedDateTime).getTime();
     const difference = futureTime - currTime;
-    if(difference > 0 ){
-      timeLeft = {
+    if (difference > 0) {
+      timeLeftObj = {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
         seconds: Math.floor((difference / 1000) % 60),
       };
-      
     }
-    
 
-    return timeLeft;
-  }
+    return timeLeftObj;
+  };
 
   const [timeLeft, setTimeLeft] = useState(calculateTime());
   useEffect(() => {
-    if(selectedDateTime === null){ //to set counter to 00 00 00 00 when date is more than 100 days, same day and past date or no date
+    if (selectedDateTime === null) {
+      //to set counter to 00 00 00 00 when date same day and past date or no date
       setTimeLeft(calculateTime());
     }
-    
+
     var intervalId = null;
-    if (selectedDateTime !== null && selectedDateTime) {// calculating time left every second using setInterval()
+    if (selectedDateTime !== null && selectedDateTime) {
+      // calculating time left every second using setInterval()
+
       intervalId = setInterval(() => {
         setTimeLeft(calculateTime());
+        if (calculateTime().seconds === 0) {
+          setIsTimerOver(true);
+          setIsTimerOn(() => false);
+          setDateTime(()=> "");
+        } else {
+          setIsTimerOver(false);
+        }
       }, 1000);
     }
-    
+
     return () => clearInterval(intervalId);
   }, [selectedDateTime]);
 
+  // const TimeisOverMsg = () => {
+  //   let secondsLeft = calculateTime().seconds;
+  //   if(secondsLeft === 0){
+  //     console.log("timer is done");
+  //   }
+  // }
+
   return (
-    <div className="timer-container">
-      <div className="timer-card"><div className="timer-info"><h1>{timeLeft.days<10?"0"+timeLeft.days:timeLeft.days}</h1><h2>Days</h2></div></div>
-      <div className="timer-card"><div className="timer-info"><h1>{timeLeft.hours<10?"0"+timeLeft.hours:timeLeft.hours}</h1><h2>Hours</h2></div></div>
-      <div className="timer-card"><div className="timer-info"><h1>{timeLeft.minutes<10?"0"+timeLeft.minutes:timeLeft.minutes}</h1><h2>Minutes</h2></div></div>
-      <div className="timer-card"><div className="timer-info"><h1>{timeLeft.seconds<10?"0"+timeLeft.seconds:timeLeft.seconds}</h1><h2>Seconds</h2></div></div>
-    </div>) 
-  
+    <>
+      {isTimerOver ? (
+        <div>TIMER KHATAM</div>
+      ) : (
+        <div className="timer-container">
+          <div className="timer-card">
+            <div className="timer-info">
+              <h1>
+                {timeLeft.days < 10 ? "0" + timeLeft.days : timeLeft.days}
+              </h1>
+              <h2>Days</h2>
+            </div>
+          </div>
+          <div className="timer-card">
+            <div className="timer-info">
+              <h1>
+                {timeLeft.hours < 10 ? "0" + timeLeft.hours : timeLeft.hours}
+              </h1>
+              <h2>Hours</h2>
+            </div>
+          </div>
+          <div className="timer-card">
+            <div className="timer-info">
+              <h1>
+                {timeLeft.minutes < 10
+                  ? "0" + timeLeft.minutes
+                  : timeLeft.minutes}
+              </h1>
+              <h2>Minutes</h2>
+            </div>
+          </div>
+          <div className="timer-card">
+            <div className="timer-info">
+              <h1>
+                {timeLeft.seconds < 10
+                  ? "0" + timeLeft.seconds
+                  : timeLeft.seconds}
+              </h1>
+              <h2>Seconds</h2>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default CountDownTimer;
--- src/components/dateTimePicker.js
+++ src/components/dateTimePicker.js
@@ -1,71 +1,97 @@
-import React from 'react'
-import { useState } from 'react';
-import './dateTimePicker.css';
-import CountDownTimer from './CountDownTimer';
+import React from "react";
+import { useState } from "react";
+import "./dateTimePicker.css";
+import CountDownTimer from "./CountDownTimer";
 function DateTimePicker() {
-
   const [selectedDateTime, setSelectedDateTime] = useState(null);
   const [dateTime, setDateTime] = useState("");
   const [isTimerOn, setIsTimerOn] = useState(false);
+  const [isPastOrNoTime, setisPastOrNoTime] = useState(false);
+  const [isMorethan100Days, setIsMoreThan100Days] = useState(false);
 
   const currTime = new Date().getTime(); //getting current time in milliseconds
-  const futureTime = new Date(dateTime).getTime();//getting future time in milliseconds
-  const differenceDays = Math.floor((futureTime - currTime)/ (1000 * 60 * 60 * 24)); //finding number of days between current and future time
-  const moreThanHundredDays = ()=> {
-    if(differenceDays>100){
+  const futureTime = new Date(dateTime).getTime(); //getting future time in milliseconds
+  const differenceDays = Math.floor(
+    (futureTime - currTime) / (1000 * 60 * 60 * 24)
+  ); //finding number of days between current and future time
+  const moreThanHundredDays = () => {
+    if (differenceDays > 100) {
       return true;
     }
     return false;
-  }
-const handleBlur = (e) =>{
+  };
+
+  const handleBlur = (e) => {
     const futureDateTime = e.target.value;
     setDateTime(futureDateTime);
+  };
+
+  const handleClear = (e) => {
+    if (e.target.value.length === 0) {
+      setDateTime(null);
+    }
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(dateTime);
+    if (moreThanHundredDays()) {
+      setIsMoreThan100Days(true);
+      setisPastOrNoTime(false);
+    } else if (dateTime === "" || differenceDays < 0) {
+      //check if dateTime currently selected is empty or past date is selected
+      console.log("are we here");
+      setIsMoreThan100Days(false);
+      setisPastOrNoTime(true);
+      setSelectedDateTime(null);
+    } else if (isTimerOn) {
+      //if timer is ON then stop it and reset the count Down
+      setIsTimerOn(false);
+      setSelectedDateTime(null);
+    } else {
+      setIsMoreThan100Days(false);
+      setisPastOrNoTime(false);
+      setIsTimerOn(true); //if timer is OFF then start it and set the count Down to the current date time selected in picker
+      setSelectedDateTime(dateTime);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <form className="date-time-form" onSubmit={handleSubmit}>
+          <input
+            className="datetime-input"
+            type="datetime-local"
+            id="datetime"
+            name="datetime"
+            onBlur={handleBlur}
+            onChange={handleClear}
+            disabled={isTimerOn}
+          />
+          <button className="datetime-btn" type="submit">
+            {isTimerOn ? "Cancel Timer" : "Start Timer"}
+          </button>
+        </form>
+
+        {isPastOrNoTime ? (
+          <h4 className="error">Past time, no time or same time is selected</h4>
+        ) : (
+          <></>
+        )}
+
+        {isMorethan100Days ? (
+          <h4 className="error">Selected time is more than 100 days</h4>
+        ) : (
+          <CountDownTimer
+            selectedDateTime={selectedDateTime}
+            setIsTimerOn={setIsTimerOn}
+            setDateTime = {setDateTime}
+          />
+        )}
+      </div>
+    </>
+  );
 }
 
-const handleClear = (e) => {
-  if(e.target.value.length === 0){
-    setDateTime(null);
-  }
-  
-}
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if(moreThanHundredDays()){  // check more than 100 days condition
-    console.log("more than 100 days");
-  }
-  else if(dateTime === "" || differenceDays < 0){ //check if dateTime currently selected is empty or past date is selected
-    console.log("Past time or No Time is selected")
-    setSelectedDateTime(null);
-  }
-  else if(isTimerOn){ //if timer is ON then stop it and reset the count Down
-    setIsTimerOn(false);
-    setSelectedDateTime(null);
-  }
-  else{
-    setIsTimerOn(true);//if timer is OFF then start it and set the count Down to the current date time selected in picker
-    setSelectedDateTime(dateTime);
-  }
-  
-  
-}
-
-  return (<>
-  <div className="reddit">
-
-    <form onSubmit={handleSubmit}>
-     <input type="datetime-local" id="datetime" name="datetime"  onBlur={handleBlur} onChange={handleClear} disabled = {isTimerOn} /> 
-     <button type='submit'>{isTimerOn?"stop":"start"}</button>
-    </form>
-  
-  
-  {/* <p>{selectedDateTime}</p> */}
-  <CountDownTimer selectedDateTime = {selectedDateTime}/>
-  
-  </div>
-
-  
-  </>
-  )
-}
-
-export default DateTimePicker
+export default DateTimePicker;
